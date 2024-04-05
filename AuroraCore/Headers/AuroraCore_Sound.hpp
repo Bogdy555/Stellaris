4f238a2056833a2620ec8035180470f9f4ddebc7
BiancaDogareci <biancadogareci@ymail.com>
2024-04-06 01:47:09 +0300

    OpenGL and Sound

--- AuroraCore/Headers/AuroraCore_Sound.hpp
+++ AuroraCore/Headers/AuroraCore_Sound.hpp
@@ -5,6 +5,9 @@
 
 
 #include "AuroraCore.hpp"
+#include <cstdint>
+#include <Windows.h>
+#include <fstream>
 
 
 
@@ -14,7 +17,41 @@ namespace AuroraCore
 	namespace Sound
 	{
 
+		class Buffer
+		{
 
+		public:
+
+			Buffer();
+			Buffer(const Buffer& _Other) = delete;
+			Buffer(Buffer&& _Other) noexcept;
+			~Buffer();
+
+			bool Create(const WAVEFORMATEX& _MetaData, const size_t _Size);
+			void Destroy();
+
+			bool LoadFromFile(const wchar_t* _Path);
+			bool LoadFromResource(const HINSTANCE _InstanceHandle, const uint16_t _ResourceId);
+			const bool SaveToFile(const wchar_t* _Path) const;
+
+			const WAVEFORMATEX GetMetaData() const;
+			const size_t GetSize() const;
+			uint8_t* GetData();
+			const uint8_t* GetData() const;
+
+			uint8_t& operator[] (const size_t _Index);
+			const uint8_t& operator[] (const size_t _Index) const;
+
+			Buffer& operator= (const Buffer& _Other) = delete;
+			Buffer& operator= (Buffer&& _Other) noexcept;
+
+		private:
+
+			WAVEFORMATEX MetaData;
+			size_t Size;
+			uint8_t* Data;
+
+		};
 
 	}
 

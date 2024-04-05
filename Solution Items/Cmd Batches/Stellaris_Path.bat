0c8146739a0143dd8d939087bbf1edaa68b62722
Bogdy555 <florescubogdan555@gmail.com>
2024-04-05 17:34:31 +0300

    Path script is now general!

--- Solution Items/Cmd Batches/Stellaris_Path.bat
+++ Solution Items/Cmd Batches/Stellaris_Path.bat
@@ -8,7 +8,7 @@ if "%SolutionDir%" == "" call vcvars64.bat
 
 echo on
 
-if "%SolutionDir%" == "" set SolutionDir=C:\Users\Bogdan\source\repos\Stellaris\
+if "%SolutionDir%" == "" set SolutionDir=%USERPROFILE%\source\repos\Stellaris\
 
 @echo off
 

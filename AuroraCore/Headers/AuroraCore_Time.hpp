3bd35e9c2ba7eb6e8f8a813905d49d939387450b
ioanapotlog <118912868+ioanapotlog@users.noreply.github.com>
2024-04-08 23:39:46 +0300

    I added the implementation for time!

--- AuroraCore/Headers/AuroraCore_Time.hpp
+++ AuroraCore/Headers/AuroraCore_Time.hpp
@@ -14,7 +14,30 @@ namespace AuroraCore
 	namespace Time
 	{
 
+		bool Init();
+		void Stop();
 
+		class Timer
+		{
+		public:
+			Timer();
+			Timer(const Timer& _Other);
+			Timer(Timer&& _Other) noexcept;
+			~Timer();
+
+			void Start();
+			void Stop();
+			void Reset();
+
+			operator const float() const;
+
+			Timer& operator= (const Timer& _Other);
+			Timer& operator= (Timer&& _Other) noexcept;
+
+		private:
+			std::chrono::system_clock::time_point Begin;
+			std::chrono::system_clock::time_point End;
+		};
 
 	}
 
